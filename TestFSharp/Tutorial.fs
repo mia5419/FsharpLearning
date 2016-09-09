1ae392f378fd6b57755d18e09483c6b1f862349a
Mia Chang <mia5419@gmail.com>
2016-09-09 18:15:06 +0800

    From Default F# Project
    Add an addition

---
+++ TestFSharp/Tutorial.fs
@@ -0,0 +1,69 @@
+﻿// This sample will guide you through elements of the F# language. 
+
+//
+// *******************************************************************************************************
+//   To execute the code in F# Interactive, highlight a section of code and press Alt-Enter in Windows or 
+//   Ctrl-Enter Mac, or right-click and select "Send Selection to F# Interactive".  
+//   You can open the F# Interactive Window from the "View" menu.
+// *******************************************************************************************************
+
+// For more about F#, see:
+//     http://fsharp.org
+//
+// For additional templates to use with F#, see the 'Online Templates' in Visual Studio,
+//     'New Project' --> 'Online Templates'
+//
+// For specific F# topics, see:
+//     http://fsharp.org (F# Open Organization)
+//     http://tryfsharp.org (F# Learning Portal)
+//     http://go.microsoft.com/fwlink/?LinkID=234174 (Visual F# Development Portal)
+//     http://go.microsoft.com/fwlink/?LinkID=124614 (Visual F# Code Gallery)
+//     http://go.microsoft.com/fwlink/?LinkId=235173 (Visual F# Math/Stats Programming)
+//     http://go.microsoft.com/fwlink/?LinkId=235176 (Visual F# Charting)
+
+
+// Contents:
+
+//    - Integers and basic functions
+//    - Booleans
+//    - Strings
+//    - Tuples
+//    - Lists and list processing
+//    - Classes
+//    - Generic classes
+//    - Implementing interfaces
+//    - Arrays
+//    - Sequences
+//    - Recursive functions
+//    - Record types
+//    - Union types
+//    - Option types           
+//    - Pattern matching       
+//    - Units of measure       
+//    - Parallel array programming
+//    - Using events
+//    - Database access using type providers
+//    - OData access using type providers
+ 
+
+// ---------------------------------------------------------------
+//         Integers and basic functions
+// ---------------------------------------------------------------
+
+module Integers =
+
+    let add x y = x + y
+    let value = add 2 2
+    // The next line prints a list that includes tuples, using %A for generic printing
+    printfn "The answer is: %d" value
+ 
+ 
+#if COMPILED
+
+module BoilerPlateForForm =
+    [<System.STAThread>]
+    do ()
+//    do System.Windows.Forms.Application.Run()
+
+#endif
+
